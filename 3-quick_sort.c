4983e56830254ecade39ec57221cc94baba2e227
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-10-24 20:22:51 -0500

    Update conditionals and low = lo

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,63 +1,63 @@
 #include "sort.h"
 
 /**
-* insertion_sort_list -  function that sorts an list of integers in ascending order
+* lomuto_partition - function Lomuto’s partition scheme.
 *
-*@list: list double linked
+* @arr: Array to process.
+*
+* @low: Pointer to first item.
+*
+* @high: Pointer to last item.
+*
+* @size: Number of items, or array size.
 *
 * Return: Nothing
 **/
 
-/*void swap(int *i, int j)
-{
-	int tmp = *i;
-	*i = *j;
-	*j = tmp;
-}*/
-
 void lomuto_partition(int *arr, int low, int high, int size)
 {
-	int pivot, lo; //= arr[high];
-	//int i = (low - 1);
+	int pivot, lo, i;
 	int aux = 0;
 	
-	if (low < high) {
-		pivot = high;
-		lo = high;
-
-	for (int i = low; i < high; i++)
+	if (low < high) 
 	{
-		if (arr[i] < arr[pivot] && i != lo)
-		{
-			/**swap(&arr[i], &arr[j]);
-			++i;*/
+		pivot = high;
+		lo = low;
 
-			aux = arr[i];
-			arr[i] = arr[lo];
-			arr[lo] = aux;
+		for (i = low; i < high; i++)
+		{
+			if (arr[i] < arr[pivot])
+			{
+				if(i != lo)
+				{
+					aux = arr[i];
+					arr[i] = arr[lo];
+					arr[lo] = aux;
+					print_array(arr, size);
+				}
+				lo++;
+			}
+		}
+		if (lo != pivot && arr[lo] != arr[pivot])
+		{
+			aux = arr[lo];
+			arr[lo] = arr[pivot];
+			arr[pivot] = aux;
 			print_array(arr, size);
 		}
-	}
-	if (lo != pivot && arr[lo] != arr[pivot])
-	{
-		aux = arr[lo];
-		arr[lo] = arr[pivot];
-		arr[pivot] = aux;
-		print_array(arr, size);
-	}
-	/**swap(&arr[i ], &arr[high]);
-	return i;*/
-	lomuto_partition(arr, low, lo - 1, size);
-	lomuto_partition(arr, high + 1, high, size);
+		lomuto_partition(arr, low, lo - 1, size);
+		lomuto_partition(arr, lo + 1, high, size);
 	}
 }
+/**
+* quick_sort - Function that sorts an array of integers in ascending order.
+*
+*@array: Array to be sorted.
+*
+* Return: Nothing
+**/
 
 void quick_sort(int *array, size_t size)
 {
-	/**if (low < high)
-	{
-		int high = high, wi = low;
-
-	}*/
 	lomuto_partition(array, 0, size - 1, size);
 }
