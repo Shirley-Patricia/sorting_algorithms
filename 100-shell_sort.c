ada860f862b6ec4468308a69e4de052d9c2e268f
Shirley <shircar16@gmail.com>
2021-10-26 08:41:23 -0500

    added conditionaland fix betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
-* bubble_sort -  function that sorts an array of integers in ascending order
+* shell_sort -  function that sorts an array of integers in ascending order
 *@array: array to order
 *@size: size of the array
 *
@@ -10,22 +10,25 @@
 
 void shell_sort(int *array, size_t size)
 {
-    size_t i, j, h;
-    int temp;
-    
-    h = 1;
-    while (h < size/3)
-        h = 3 * h + 1;
-    while (h >= 1)
-    {
-        for (i = h; i < size; i++)
-        {
-          temp = array[i];
-          for (j = i; j >= h && array[j - h] > temp; j -= h)
-            array[j] = array[j - h];
-          array[j] = temp;
-        }
-        h /= 3;
-        print_array(array, size);
-    }
+	size_t i, j, h;
+	int temp;
+
+	if (array == NULL || size == 0)
+		return;
+
+	h = 1;
+	while (h < size / 3)
+		h = 3 * h + 1;
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= h && array[j - h] > temp; j -= h)
+				array[j] = array[j - h];
+			array[j] = temp;
+		}
+		h /= 3;
+		print_array(array, size);
+	}
 }
